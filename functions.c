fa80bc025e897a2505d87111d18d64a1ceed05a6
AlvieTheGreat <141351204+AlvieTheGreat@users.noreply.github.com>
2023-11-14 16:04:20 +0300

    Update functions.c

--- functions.c
+++ functions.c
@@ -3,14 +3,14 @@
 /************************* PRINT CHAR *************************/
 
 /**
- * print_char - Prints a char
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
+ * print_char - Prints a character.
+ * @types: List of arguments
+ * @buffer: Memory allocated to handle print.
  * @flags:  Calculates active flags
- * @width: Width
- * @precision: Precision specification
+ * @width: length of character per space.
+ * @precision: decimal place specification.
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: The character(s).
  */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -22,13 +22,13 @@ int print_char(va_list types, char buffer[],
 /************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
+ * @types: List of arguments
+ * @buffer: Memory allocated to handle print
  * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
+ * @width: get length of string per character.
+ * @precision: decimal place specification
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: The string.
  */
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -77,13 +77,13 @@ int print_string(va_list types, char buffer[],
 /************************* PRINT PERCENT SIGN *************************/
 /**
  * print_percent - Prints a percent sign
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
+ * @types: List of arguments
+ * @buffer: Memory allocated to handle print
  * @flags:  Calculates active flags
  * @width: get width.
- * @precision: Precision specification
+ * @precision: Decimal place specification
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: The percent sign.
  */
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -99,14 +99,14 @@ int print_percent(va_list types, char buffer[],
 
 /************************* PRINT INT *************************/
 /**
- * print_int - Print int
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
+ * print_int - Prints an integer.
+ * @types: List of arguments
+ * @buffer: Memory allocated to handle print.
  * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
+ * @width: Specify length of the integer.
+ * @precision: Decimal place specification.
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: The integer/number.
  */
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -144,13 +144,13 @@ int print_int(va_list types, char buffer[],
 /************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
+ * @types: List of arguments
+ * @buffer: Memory allocated to handle print
  * @flags:  Calculates active flags
- * @width: get width.
- * @precision: Precision specification
+ * @width: specify length of the binary.
+ * @precision: Decimal place specification
  * @size: Size specifier
- * Return: Numbers of char printed.
+ * Return: The binary number.
  */
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
