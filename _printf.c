dda42eb00ecedd480e85d21f28e4652d008daded
AlvieTheGreat <141351204+AlvieTheGreat@users.noreply.github.com>
2023-11-14 15:17:31 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,9 +3,9 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * _printf - produces output according to a format.
  * @format: format.
- * Kenneth and Mercy Team
+ * Kennedy and Mercy Team.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
@@ -54,7 +54,7 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints the contents of the buffer if it exists.
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
