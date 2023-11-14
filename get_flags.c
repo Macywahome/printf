f8e52e45ceaf9bffac3bf2531f91e04977bf706e
AlvieTheGreat <141351204+AlvieTheGreat@users.noreply.github.com>
2023-11-14 21:12:06 +0300

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -2,6 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
+ * Kennedy and Mercy group project.
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
